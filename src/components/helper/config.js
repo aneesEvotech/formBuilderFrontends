ecec66495f44f2b61bb425715655e8488b7b5cf4
aneesEvotech <anees@evotech.global>
2025-06-09 11:56:44 +0530

    paywall subscription

--- src/components/helper/config.js
+++ src/components/helper/config.js
@@ -1,3 +1,3 @@
-export const base_urlLink = 'http://localhost:3001';
-// export const base_urlLink = 'https://formbuilderbackend-1cgr.onrender.com';
+// export const base_urlLink = 'http://localhost:3001';
+export const base_urlLink = 'https://formbuilderbackend-1cgr.onrender.com';
 // export const base_urlLink = 'https://form-builder-frontends-xmit.vercel.app';
