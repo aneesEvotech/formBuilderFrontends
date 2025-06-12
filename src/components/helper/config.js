557a0c6136edb8dc849d0f39085c5387e0e35ee2
aneesEvotech <anees@evotech.global>
2025-06-12 12:06:22 +0530

    changes in login and log out

--- src/components/helper/config.js
+++ src/components/helper/config.js
@@ -1,3 +1,2 @@
-export const base_urlLink = 'http://localhost:3001';
-// export const base_urlLink = 'https://formbuilderbackend-1cgr.onrender.com';
-// export const base_urlLink = 'https://form-builder-frontends-xmit.vercel.app';
+// export const base_urlLink = 'http://localhost:3001';
+export const base_urlLink = 'https://formbuilderbackend-1cgr.onrender.com';
